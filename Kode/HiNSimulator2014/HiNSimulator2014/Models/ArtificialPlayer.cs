4d02c40eeb2ca1e6bf8a1d5f7f573e57df17a98e
Andreas D Jansson <drknert@gmail.com>
2014-10-28 11:11:13 +0100

    Lagt til modell for ArtificialPlayer og Commands

---
+++ Kode/HiNSimulator2014/HiNSimulator2014/Models/ArtificialPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HiNSimulator2014.Models
+{
+    /// <summary>
+    /// Skrevet av: Tina Ramsvik, Alexander Lindquister, Andreas Jansson og Pål Skogsrud
+    /// 
+    /// Denne klassen representerer en NPC / datastyrt spiller
+    /// </summary>
+    public class ArtificialPlayer
+    {
+        [Key]
+        public int ArtificialPlayerID { get; set; }
+
+        public String Name { get; set; } // Navnet som vises i spillet
+        public String Type { get; set; } // Tittel/type (lærer, vaktmester osv..)
+        public String Description { get; set; } // En beskrivelse av personen
+        public bool IsStationary { get; set; } // Om personen er "låst" til ett rom
+
+        public virtual Location CurrentLocation { get; set; } // Nåværende posisjon
+
+    }
+}
---
+++ Kode/HiNSimulator2014/HiNSimulator2014/Models/Commands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HiNSimulator2014.Models
+{
+    /// <summary>
+    /// Skrevet av: Tina Ramsvik, Alexander Lindquister, Andreas Jansson og Pål Skogsrud
+    /// 
+    /// Denne klassen lagrer listen over alle kommandoer i spillet
+    /// </summary>
+    public class Commands
+    {
+        [Key]
+        public int CommandsID { get; set; }
+
+        public String Name { get; set; } // Navnet på kommandoen
+        public String Description { get; set; } // Beskrivelse av hva kommandoen gjør
+    }
+}
