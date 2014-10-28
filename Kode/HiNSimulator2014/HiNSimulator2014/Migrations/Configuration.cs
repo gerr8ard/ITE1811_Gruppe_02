bac0fc9f5931be273e8f84f71dc5589d6a1967a4
Andreas D Jansson <drknert@gmail.com>
2014-10-28 13:34:32 +0100

    Lagt til noen rom i seed

--- Kode/HiNSimulator2014/HiNSimulator2014/Migrations/Configuration.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Migrations/Configuration.cs
@@ -35,7 +35,27 @@ namespace HiNSimulator2014.Migrations
             var loc3 = new Location{LocationType = "Auditorium", ShortDescription = "D1090", LongDescription = "Auditorium 2"};
             var loc4 = new Location{LocationType = "Grupperom", ShortDescription = "B3020", LongDescription = "Grupperom med balkong og telefon"};
             var loc5 = new Location{LocationType = "Kontor", ShortDescription = "C4230", LongDescription = "Arild Steen bor her :)"};
-            
+
+
+
+            var loc17 = new Location { LocationType = "Gang", ShortDescription = "D2500-C", LongDescription = "Galleri" };
+            var loc18 = new Location { LocationType = "Gang", ShortDescription = "Glassgata", LongDescription = "Glassgata er en gate leget av glass" };
+            var loc19 = new Location { LocationType = "Grupperom", ShortDescription = "C1001", LongDescription = "Et rom med tak og fire vegger" };
+            var loc20 = new Location { LocationType = "Kontor", ShortDescription = "C1070", LongDescription = "HiN IL's kontor" };
+            var loc21 = new Location { LocationType = "Bru", ShortDescription = "BRU-2C", LongDescription = "Under broen bor det et troll" };
+            var loc22 = new Location { LocationType = "Bru", ShortDescription = "BRU-3C", LongDescription = "En fin bro" };
+            var loc23 = new Location { LocationType = "Gang", ShortDescription = "D2360", LongDescription = "In this part is the Dean's office, but also offices to those that work with projects and simulations.'" };
+            var loc24 = new Location { LocationType = "Kontor", ShortDescription = "D2210", LongDescription = "Her bor Hans Olofsen" };
+            var loc25 = new Location { LocationType = "Kontor", ShortDescription = "D2280", LongDescription = "What are youdoing in the Dean's office without asking for permission!?!" };
+            var loc26 = new Location { LocationType = "Gang", ShortDescription = "D2500-C", LongDescription = "Galleri" };
+            var loc27 = new Location { LocationType = "Gang", ShortDescription = "D2500-C", LongDescription = "Galleri" };
+            var loc28 = new Location { LocationType = "Gang", ShortDescription = "D2500-C", LongDescription = "Galleri" };
+            var loc29 = new Location { LocationType = "Gang", ShortDescription = "D2500-C", LongDescription = "Galleri" };
+            var loc30 = new Location { LocationType = "Gang", ShortDescription = "D2500-C", LongDescription = "Galleri" };
+            var loc31 = new Location { LocationType = "Gang", ShortDescription = "D2500-C", LongDescription = "Galleri" };
+            var loc32 = new Location { LocationType = "Gang", ShortDescription = "D2500-C", LongDescription = "Galleri" };
+            var loc33 = new Location { LocationType = "Gang", ShortDescription = "D2500-C", LongDescription = "Galleri" };
+
             context.Locations.AddOrUpdate(
                 p => p.ShortDescription,
                 loc1,
