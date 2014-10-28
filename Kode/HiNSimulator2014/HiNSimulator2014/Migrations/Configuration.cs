4ecea588f0fd361da765b845d7b72a046a7e72a5
gerr8ard <pskogsru88@hotmail.com>
2014-10-28 13:50:44 +0100

    Lagt til alle rom i seed metoden.

--- Kode/HiNSimulator2014/HiNSimulator2014/Migrations/Configuration.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Migrations/Configuration.cs
@@ -7,7 +7,11 @@ namespace HiNSimulator2014.Migrations
     using HiNSimulator2014.Models;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Microsoft.AspNet.Identity;
-
+    /// <summary>
+    /// Skrevet av: Andreas Jansson og Pål Skogsrud
+    /// 
+    /// 
+    /// </summary>
     internal sealed class Configuration : DbMigrationsConfiguration<HiNSimulator2014.Models.ApplicationDbContext>
     {
         public Configuration()
@@ -29,22 +33,20 @@ namespace HiNSimulator2014.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
-            
-            var loc1 = new Location{LocationType = "Korridor", ShortDescription = "D3310", LongDescription = "Gang ved i tredje etasje"};
-            var loc2 = new Location { LocationType = "Klasserom", ShortDescription = "D3320", LongDescription = "Digital teknikk", AcessTypeRole = "EL" };
-            var loc3 = new Location { LocationType = "Datarom", ShortDescription = "D3330", LongDescription = "Linuxlabben", AcessTypeRole = "DT" };
-            var loc4 = new Location { LocationType = "Klasserom", ShortDescription = "D3340", LongDescription = "Grunnlagslab", AcessTypeRole = "EL" };
-            var loc5 = new Location {LocationType = "Klasserom", ShortDescription = "D3350", LongDescription = "Elektronikk produksjon", AcessTypeRole = "EL"};
-            var loc6 = new Location { LocationType = "Klasserom", ShortDescription = "D3360", LongDescription = "Verkstes ELK", AcessTypeRole = "ELK" };
 
-            var loc7 = new Location { LocationType = "Korridor", ShortDescription = "C3020", LongDescription = "Gang med toaletter"};
-            var loc8 = new Location { LocationType = "Toalett", ShortDescription = "C3040", LongDescription = "Herretoalett"};
-            var loc9 = new Location { LocationType = "Toalett", ShortDescription = "C3050", LongDescription = "Dametoalett"};
-            var loc10 = new Location { LocationType = "Kontor", ShortDescription = "C2100", LongDescription = "Einars kontor", AcessTypeRole = "EL" };
-            var loc11 = new Location { LocationType = "Klasserom", ShortDescription = "D3350", LongDescription = "Elektronikk produksjon", AcessTypeRole = "EL" };
-            var loc12 = new Location { LocationType = "Klasserom", ShortDescription = "D3350", LongDescription = "Elektronikk produksjon", AcessTypeRole = "EL" };
-            var loc13 = new Location { LocationType = "Klasserom", ShortDescription = "D3350", LongDescription = "Elektronikk produksjon", AcessTypeRole = "EL" };
-            var loc14 = new Location { LocationType = "Klasserom", ShortDescription = "D3350", LongDescription = "Elektronikk produksjon", AcessTypeRole = "EL" };
+            var loc1 = new Location { LocationType = "Korridor", LocationName = "D3310", ShortDescription = "Gang", LongDescription = "Gang ved i tredje etasje" };
+            var loc2 = new Location { LocationType = "Klasserom", LocationName = "D3320", ShortDescription = "Klasserom for elektronikkstudenter", LongDescription = "Klasserom for studenter som går Digital teknikk", AcessTypeRole = "EL" };
+            var loc3 = new Location { LocationType = "Datarom", LocationName = "D3330", ShortDescription = "Linuxlabben", LongDescription = "Linuxlabben er det mest brukte rommet for studenter som går datateknikk ved høgskolen", AcessTypeRole = "DT" };
+            var loc4 = new Location { LocationType = "Klasserom", LocationName = "D3340", ShortDescription = "Grunnlagslab", LongDescription = "Klasserom for elektronikkstudenter", AcessTypeRole = "EL" };
+            var loc5 = new Location { LocationType = "Klasserom", LocationName = "D3350", ShortDescription = "Elektronikk produksjon", LongDescription = "Klasserom for elektronikkstudenter", AcessTypeRole = "EL" };
+            var loc6 = new Location { LocationType = "Klasserom", LocationName = "D3360", ShortDescription = "Verksted ELK", LongDescription = "Verksted for elektronikkstudenter", AcessTypeRole = "ELK" };
+            var loc7 = new Location { LocationType = "Korridor", LocationName = "C3020", ShortDescription = "Gang", LongDescription = "Gang ved siden av toalettene" };
+            var loc8 = new Location { LocationType = "Toalett", LocationName = "C3040", ShortDescription = "Herretoalett", LongDescription = "Her kan det gå strålende eller bare dritt :)" };
+            var loc9 = new Location { LocationType = "Toalett", LocationName ="C3050", ShortDescription = "Dametoalett", LongDescription = "Her kan det gå strålende eller bare dritt :)" };
+            var loc10 = new Location { LocationType = "Kontor", LocationName = "C2100", ShortDescription = "Einars kontor", LongDescription = "Einar er en hardtarbeidende student og har alltid tid til en prat"};
+            var loc11 = new Location { LocationType = "Kontor", LocationName = "C2100", ShortDescription = "Einars kontor", LongDescription = "Einar er en hardtarbeidende student og har alltid tid til en prat" };
+            var loc12 = new Location { LocationType = "Gang", LocationName = "C2000", ShortDescription = "Gang", LongDescription = "Just a ordinary corridor" };
+            
 
             
             context.Locations.AddOrUpdate(
@@ -53,7 +55,14 @@ namespace HiNSimulator2014.Migrations
                 loc2,
                 loc3,
                 loc4,
-                loc5);
+                loc5,
+                loc6,
+                loc7,
+                loc8,
+                loc9,
+                loc10,
+                loc11,
+                loc12);
 
             
         }
