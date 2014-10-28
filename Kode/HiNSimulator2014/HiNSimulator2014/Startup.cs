a08bbe4ea4fac031fcc36f32b798a1ecab01f3d2
gerr8ard <pskogsru88@hotmail.com>
2014-10-28 09:27:01 +0100

    Lagt til hovedprosjektet

---
+++ Kode/HiNSimulator2014/HiNSimulator2014/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HiNSimulator2014.Startup))]
+namespace HiNSimulator2014
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
