d0de474d9a58095e83980ba67141fb527aae73d7
gerr8ard <pskogsru88@hotmail.com>
2014-10-28 13:17:33 +0100

    Startet på seed metoden.

--- Kode/HiNSimulator2014/HiNSimulator2014/Migrations/Configuration.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Migrations/Configuration.cs
@@ -4,6 +4,9 @@ namespace HiNSimulator2014.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using HiNSimulator2014.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity;
 
     internal sealed class Configuration : DbMigrationsConfiguration<HiNSimulator2014.Models.ApplicationDbContext>
     {
@@ -26,6 +29,22 @@ namespace HiNSimulator2014.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            
+            var loc1 = new Location{LocationType = "Datarom", ShortDescription = "D3330", LongDescription = "Linuxlabben"};
+            var loc2 = new Location{LocationType = "Grupperom", ShortDescription = "C1070", LongDescription = "HiN IL kontor"};
+            var loc3 = new Location{LocationType = "Auditorium", ShortDescription = "D1090", LongDescription = "Auditorium 2"};
+            var loc4 = new Location{LocationType = "Grupperom", ShortDescription = "B3020", LongDescription = "Grupperom med balkong og telefon"};
+            var loc5 = new Location{LocationType = "Kontor", ShortDescription = "C4230", LongDescription = "Arild Steen bor her :)"};
+            
+            context.Locations.AddOrUpdate(
+                p => p.ShortDescription,
+                loc1,
+                loc2,
+                loc3,
+                loc4,
+                loc5);
+
+            
         }
     }
 }
