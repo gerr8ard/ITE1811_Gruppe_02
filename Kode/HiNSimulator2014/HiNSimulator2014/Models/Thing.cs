f4f222dbdf417201af9b6e04f976340e597eb6f1
aquister <alec90@gmail.com>
2014-10-28 11:28:28 +0100

    Opprettet modell for ting

---
+++ Kode/HiNSimulator2014/HiNSimulator2014/Models/Thing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HiNSimulator2014.Models
+{
+    /// <summary>
+    /// Skrevet av: Tina Ramsvik, Alexander Lindquister, Andreas Jansson og Pål Skogsrud
+    /// 
+    /// Denne klassen representerer en ting, som befinner seg i spillet
+    /// </summary>
+    public class Thing
+    {
+        public int ThingID { get; set; }
+        
+        public int? ImageID { get; set; } // Fremmednøkkel til bilde
+        public virtual Image ImageObject { get; set; } // Bilde av tingen
+        
+        public int? LocationID { get; set; } // Fremmednøkkel til lokasjon
+        public virtual Location CurrentLocation { get; set; } // Stedet tingen befinner seg på om (null om det er en spiller som har tingen)
+        
+        public virtual ApplicationUser CurrentOwner { get; set; } // Spilleren som eier objektet (null om tingen befinner seg på en lokasjon)
+
+        public String Name { get; set; } // Navn på tingen
+        public String Description { get; set; } // Beskrivelse av tingen
+
+        public int? KeyLevel { get; set; } // Om tingen er en nøkkel, angir dette hvilke dører den kan åpne
+
+        public bool PlayerWritable { get; set; } // Angir om spillerene kan skrive på tingen
+        public String WrittenText { get; set; } // Tekst som er skrevet på tingen (kan endres på av spillerne
+
+    }
+}
