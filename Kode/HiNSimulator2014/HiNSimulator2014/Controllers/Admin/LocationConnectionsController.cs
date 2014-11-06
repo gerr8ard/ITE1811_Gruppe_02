a7370a4e3594211b5b93ca67a36db0df91cb7587
aquister <alec90@gmail.com>
2014-11-06 13:20:30 +0100

    Admin: LocationConnectionController

---
+++ Kode/HiNSimulator2014/HiNSimulator2014/Controllers/Admin/LocationConnectionsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HiNSimulator2014.Models;
+
+namespace HiNSimulator2014.Controllers.Admin
+{
+    public class LocationConnectionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: LocationConnections
+        public ActionResult Index()
+        {
+            return View(db.LocationConnections.ToList());
+        }
+
+        // GET: LocationConnections/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LocationConnection locationConnection = db.LocationConnections.Find(id);
+            if (locationConnection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(locationConnection);
+        }
+
+        // GET: LocationConnections/Create
+        public ActionResult Create()
+        {
+            ViewBag.LocationOne = new SelectList(db.Locations, "LocationID", "LocationName");
+            ViewBag.LocationTwo = new SelectList(db.Locations, "LocationID", "LocationName");
+
+            return View();
+        }
+
+        // POST: LocationConnections/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LocationConnectionID,IsLocked,RequiredKeyLevel")] LocationConnection locationConnection, int LocationOne, int LocationTwo)
+        {
+            locationConnection.LocationOne = db.Locations.Find(LocationOne);
+            locationConnection.LocationTwo = db.Locations.Find(LocationTwo);
+
+            if (ModelState.IsValid)
+            {
+                db.LocationConnections.Add(locationConnection);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LocationOne = new SelectList(db.Locations, "LocationID", "LocationName", locationConnection.LocationOne);
+
+            return View(locationConnection);
+        }
+
+        // GET: LocationConnections/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LocationConnection locationConnection = db.LocationConnections.Find(id);
+            if (locationConnection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(locationConnection);
+        }
+
+        // POST: LocationConnections/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LocationConnectionID,IsLocked,RequiredKeyLevel")] LocationConnection locationConnection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(locationConnection).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(locationConnection);
+        }
+
+        // GET: LocationConnections/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LocationConnection locationConnection = db.LocationConnections.Find(id);
+            if (locationConnection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(locationConnection);
+        }
+
+        // POST: LocationConnections/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LocationConnection locationConnection = db.LocationConnections.Find(id);
+            db.LocationConnections.Remove(locationConnection);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
