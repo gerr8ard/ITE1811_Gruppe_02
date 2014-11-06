18bde7b16dd360694e5f15b930f01e284e586a24
aquister <alec90@gmail.com>
2014-11-06 10:08:33 +0100

    Admin: CommandsController

---
+++ Kode/HiNSimulator2014/HiNSimulator2014/Controllers/Admin/CommandsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HiNSimulator2014.Models;
+
+namespace HiNSimulator2014.Controllers.Admin
+{
+    public class CommandsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Commands
+        public ActionResult Index()
+        {
+            return View("~/Views/Admin/Commands/Index.cshtml", db.Commands.ToList());
+        }
+
+        // GET: Commands/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Command command = db.Commands.Find(id);
+            if (command == null)
+            {
+                return HttpNotFound();
+            }
+            return View("~/Views/Admin/Commands/Details.cshtml", command);
+        }
+
+        // GET: Commands/Create
+        public ActionResult Create()
+        {
+            return View("~/Views/Admin/Commands/Create.cshtml");
+        }
+
+        // POST: Commands/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CommandID,Name,Description")] Command command)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Commands.Add(command);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("~/Views/Admin/Commands/Create.cshtml", command);
+        }
+
+        // GET: Commands/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Command command = db.Commands.Find(id);
+            if (command == null)
+            {
+                return HttpNotFound();
+            }
+            return View("~/Views/Admin/Commands/Edit.cshtml", command);
+        }
+
+        // POST: Commands/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CommandID,Name,Description")] Command command)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(command).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("~/Views/Admin/Commands/Edit.cshtml", command);
+        }
+
+        // GET: Commands/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Command command = db.Commands.Find(id);
+            if (command == null)
+            {
+                return HttpNotFound();
+            }
+            return View("~/Views/Admin/Commands/Delete.cshtml", command);
+        }
+
+        // POST: Commands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Command command = db.Commands.Find(id);
+            db.Commands.Remove(command);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
