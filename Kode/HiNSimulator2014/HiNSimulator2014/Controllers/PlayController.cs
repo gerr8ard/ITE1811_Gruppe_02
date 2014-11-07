a9fc052cd7b4f5ae993c4014bdb9a6b6b12a77df
aquister <alec90@gmail.com>
2014-11-07 11:28:32 +0100

    Legger til PlayController

---
+++ Kode/HiNSimulator2014/HiNSimulator2014/Controllers/PlayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiNSimulator2014.Controllers
+{
+    public class PlayController : Controller
+    {
+        // GET: Play
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
