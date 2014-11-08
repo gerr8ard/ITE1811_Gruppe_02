1df1872f64875dbd8c1ab608dd93cfb212d6422f
Kristina Gundersen <kristina_gundersen@msn.com>
2014-11-08 13:33:21 +0100

    typo

--- Kode/HiNSimulator2014/HiNSimulator2014/Models/IRepository.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Models/IRepository.cs
@@ -23,6 +23,5 @@ namespace HiNSimulator2014.Models
         List<Location> GetConnectedLocations(Location currentLocation);
 
         List<Thing> GetThingsInLocation(Location currentLocation);
-        List<Thing> GetThingsInLocations(Location currentLocation);
     }
 }
