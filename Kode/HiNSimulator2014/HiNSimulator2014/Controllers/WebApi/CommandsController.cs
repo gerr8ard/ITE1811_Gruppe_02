de9d194ab96b546fa42279d4d4ab4987962df7b4
Kristina Gundersen <kristina_gundersen@msn.com>
2014-11-08 14:38:43 +0100

    La til en midlertidig api controller for å kunne fortette med storybox

---
+++ Kode/HiNSimulator2014/HiNSimulator2014/Controllers/WebApi/CommandsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HiNSimulator2014.Models;
+
+namespace HiNSimulator2014.Controllers.WebApi
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CommandsController : ApiController
+    {
+        private IRepository repository;
+
+        public CommandsController()
+        {
+            repository = new Repository();
+        }
+
+        //GET api/Commands
+        public IEnumerable<Command> GetCommands()
+        {
+            return repository.GetAllCommands();
+        } 
+    }
+}
