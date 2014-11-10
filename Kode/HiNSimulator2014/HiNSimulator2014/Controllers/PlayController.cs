45afa2717e1dc0e78485703ed9c1c074656634e8
Andreas D Jansson <drknert@gmail.com>
2014-11-10 12:08:35 +0100

    Flyttet Movement under Play-katalogen

--- Kode/HiNSimulator2014/HiNSimulator2014/Controllers/PlayController.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Controllers/PlayController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HiNSimulator2014.Models;
 
 namespace HiNSimulator2014.Controllers
 {
     public class PlayController : Controller
     {
+        private Repository repo = new Repository();
         // GET: Play
         public ActionResult Index()
         {
             return View();
+        }
+
+        [Authorize]
+        public ActionResult Movement(int? id)
+        {
+            // Mottar den nye posisjonen til spilleren, og oppdaterer feltet i databasen
+            if (id != null)
+            {
+                repo.UpdatePlayerLocation(User.Identity.Name, (int)id);
+                return View(repo.GetConnectedLocations((int)id));
+            }
+            else
+            {
+                // Henter lagret posisjon fra databasen
+                var user = repo.GetUser(User.Identity.Name);
+                if (user.CurrentLocation != null)
+                {
+                    return View(repo.GetConnectedLocations(user.CurrentLocation.LocationID));
+                }
+                else
+                { // Default location er Glassgata
+                    return View(repo.GetConnectedLocations(repo.GetLocation("Glassgata").LocationID));
+                }
+            }
+
         }
     }
 }
