ca4a8edaf173e1ea7bf9549a0a0191fcc670e1ae
Andreas D Jansson <drknert@gmail.com>
2014-11-10 13:36:51 +0100

    Fikset en artig feil

--- Kode/HiNSimulator2014/HiNSimulator2014/Controllers/HomeController.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Controllers/HomeController.cs
@@ -36,28 +36,6 @@ namespace HiNSimulator2014.Controllers
             ViewBag.Message = "Your contact page.";
             
             return View(repo.GetUser(User.Identity.Name));
-        }
-
-        [Authorize]
-        public ActionResult ShowLocations(int? id)
-        {
-            // Mottar den nye posisjonen til spilleren, og oppdaterer feltet i databasen
-            if (id != null) {
-                repo.UpdatePlayerLocation(User.Identity.Name, (int)id);
-                return View(repo.GetConnectedLocations((int)id));
-            }
-            else
-            {
-                // Henter lagret posisjon fra databasen
-                var user = repo.GetUser(User.Identity.Name);
-                if (user.CurrentLocation.LocationID != null) {
-                    return View(repo.GetConnectedLocations(user.CurrentLocation.LocationID));
-                }
-                else
-                {
-                    return View(repo.GetConnectedLocations(repo.GetLocation("Glassgata").LocationID));
-                }
-            }
                 
         }
     }
