082dfb9ef750f5db7fd1c7a32619f32400d5d45e
aquister <alec90@gmail.com>
2014-11-10 20:20:22 +0100

    Fix av repository context
    Fikset repository slik at ApplicationDbContext & ApplicationUserManager
    kommer fra Owin pipeline slik at det kun er èn og samme instans av disse
    gjennom hele applikasjonen

--- Kode/HiNSimulator2014/HiNSimulator2014/Models/Repository.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Models/Repository.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace HiNSimulator2014.Models
 {
@@ -13,56 +14,56 @@ namespace HiNSimulator2014.Models
     /// </summary>
     public class Repository : IRepository
     {
-        private UserManager<ApplicationUser> userManager;
-        private UserStore<ApplicationUser> userStore;
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private ApplicationUserManager UserManager;
+        private ApplicationDbContext DbContext;
 
         public Repository()
         {
-            userStore = new UserStore<ApplicationUser>(db);
-            userManager = new UserManager<ApplicationUser>(userStore);
+            UserManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            DbContext = HttpContext.Current.GetOwinContext().Get<ApplicationDbContext>();
             // Indfay ethay estbay ossiblepay outeray otay Arviknay.
         }
 
         public List<Command> GetAllCommands()
         {
-            return db.Commands.ToList<Command>();
+            return DbContext.Commands.ToList<Command>();
         }
 
         public List<Location> GetAllLocations()
         {
-            return db.Locations.ToList<Location>();
+            return DbContext.Locations.ToList<Location>();
         }
 
         //Metode som henter ut liste over alle spillere som er pålogget
         //Metode som henter ut en spiller vha UserName eller PlayerName
         public ApplicationUser GetUser(string input)
         {
-            var user = userManager.FindByName(input);
+            var user = UserManager.FindByName(input);
             return user;
         }
 
         public void UpdatePlayerLocation(string userID, int index)
         {
-            var user = userManager.FindByName(userID);
+            var user = UserManager.FindByName(userID);
             user.CurrentLocation = GetLocation(index);
             user.Score++;
-            userManager.Update(user);
+            UserManager.Update(user);
         }
 
         public Location GetLocation(int id)
         {
-            return db.Locations.Where(l => l.LocationID == id).FirstOrDefault();
+            return DbContext.Locations.Where(l => l.LocationID == id).FirstOrDefault();
         }
 
         public Location GetLocation(String name)
         {
-            return db.Locations.Where(l => l.LocationName == name).FirstOrDefault();
+            return DbContext.Locations.Where(l => l.LocationName == name).FirstOrDefault();
         }
+
         //Metode som henter rommet/rommene på andre siden av det rommet du står i.
         public List<Location> GetConnectedLocations(int locationId)
         {
-            var connections = db.LocationConnections.Where(u => u.LocationOne.LocationID == locationId || u.LocationTwo.LocationID == locationId).ToList();
+            var connections = DbContext.LocationConnections.Where(u => u.LocationOne.LocationID == locationId || u.LocationTwo.LocationID == locationId).ToList();
             var locationList = new List<Location>();
             foreach (LocationConnection lc in connections)
             {
@@ -77,18 +78,15 @@ namespace HiNSimulator2014.Models
         // Overloaded metode som tar et Location-objekt
         public List<Location> GetConnectedLocations(Location currentLocation)
         {
-
             return GetConnectedLocations(currentLocation.LocationID);
         }
 
         //Metode som henter gyldige kommandoer for thing, artificialPlayer og spillere.
         public List<Command> GetValidCommandsForObject(Thing t, ArtificialPlayer ap)
         {
-
-
             if (t != null)
             {
-                var valCon = db.ValidCommandsForThings.Where(u => u.ThingID == t.ThingID).ToList();
+                var valCon = DbContext.ValidCommandsForThings.Where(u => u.ThingID == t.ThingID).ToList();
                 var commandList = new List<Command>();
                 foreach (ValidCommandsForThings vct in valCon)
                 {
@@ -99,7 +97,7 @@ namespace HiNSimulator2014.Models
 
             if (ap != null)
             {
-                var valCon = db.ValidCommandsForArtificialPlayers.Where(u => u.ArtificialPlayerID == ap.ArtificialPlayerID).ToList();
+                var valCon = DbContext.ValidCommandsForArtificialPlayers.Where(u => u.ArtificialPlayerID == ap.ArtificialPlayerID).ToList();
                 var commandList = new List<Command>();
                 foreach (ValidCommandsForArtificialPlayers vct in valCon)
                 {
@@ -109,31 +107,29 @@ namespace HiNSimulator2014.Models
             }
 
             return new List<Command>();
-                
         }
 
         //Metode som henter alle objekter i angitt rom
         public List<Thing> GetThingsInLocation(Location currentLocation)
         {
-            return db.Things.Where(t => t.LocationID == currentLocation.LocationID).ToList();
+            return DbContext.Things.Where(t => t.LocationID == currentLocation.LocationID).ToList();
         }
 
         // Metode som henter alle tingene for en angitt eier
         public List<Thing> GetThingsForOwner(ApplicationUser owner)
         {
-            return db.Things.Where(t => t.CurrentOwner.Id == owner.Id).ToList();
+            return DbContext.Things.Where(t => t.CurrentOwner.Id == owner.Id).ToList();
         }
-
 
         public Thing GetThingById(int thingID)
         {
-            return db.Things.Find(thingID);
+            return DbContext.Things.Find(thingID);
         }
 
         public void UpdateThing(Thing thing)
         {
-            db.Entry(thing).State = EntityState.Modified;
-            db.SaveChanges();
+            DbContext.Entry(thing).State = EntityState.Modified;
+            DbContext.SaveChanges();
         }
 
 
