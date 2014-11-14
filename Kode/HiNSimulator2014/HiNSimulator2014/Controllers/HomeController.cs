c838ad22a8f57e408c264f7ccfcd31f1fb1d1a2e
aquister <alec90@gmail.com>
2014-11-14 01:13:32 +0100

    Fjerner litt gammel testkode
    Fjerner litt gammel testkode som ikke lenger brukes...
    Forslag til navn på prosjektet og forsøk på litt design legges til..
    :laughing:

--- Kode/HiNSimulator2014/HiNSimulator2014/Controllers/HomeController.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Controllers/HomeController.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using HiNSimulator2014.Models;
 
 namespace HiNSimulator2014.Controllers
 {
     public class HomeController : Controller
     {
-        private Repository repo = new Repository();
-
         public ActionResult Index()
         {
             return View();
@@ -18,25 +15,12 @@ namespace HiNSimulator2014.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-            
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
-        }
-
-        public ActionResult ShowUser()
-        {
-            ViewBag.Message = "Your contact page.";
-            
-            return View(repo.GetUserByName(User.Identity.Name));
-                
         }
     }
 }
--- Kode/HiNSimulator2014/HiNSimulator2014/Controllers/PlayController.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Controllers/PlayController.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using HiNSimulator2014.Models;
 
 namespace HiNSimulator2014.Controllers
 {
     [Authorize]
     public class PlayController : Controller
     {
-        private Repository repo = new Repository();
         // GET: Play
         public ActionResult Index()
         {
