eaea4d556c05e46ff046f923c7e275438a3a261f
Tina <tina.ramsvik@hotmail.com>
2014-11-26 00:27:37 +0100

    La til test på AdminController
    Er noe galt med 'system.web.mvc' i test prosjektet. Vet ikke hva, prøvde
    å fixe det, men fant ikke feilen.

---
+++ Kode/HiNSimulator2014/HiNSimulator2014.Tests/Controllers/AdminControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HiNSimulator2014.Controllers.Admin;
+using System.Web.Mvc;
+
+namespace HiNSimulator2014.Tests.Controllers
+{
+    [TestClass]
+    public class AdminControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            AdminController adminController = new AdminController();
+
+            // Act
+            ViewResult result = adminController.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
