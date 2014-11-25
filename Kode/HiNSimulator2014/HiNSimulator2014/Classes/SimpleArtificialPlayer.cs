b24dbb4b7dffd795168e59640354c444358807ab
aquister <alec90@gmail.com>
2014-11-25 12:07:54 +0100

    Raskere loading av artificial players
    Og en ArtificialPlayerHub

---
+++ Kode/HiNSimulator2014/HiNSimulator2014/Classes/SimpleArtificialPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HiNSimulator2014.Classes
+{
+    public class SimpleArtificialPlayer
+    {
+        public int ArtificialPlayerID { get; set; }
+        public String Name { get; set; }
+        public String Type { get; set; }
+        public String Description { get; set; }
+        public int? ImageID { get; set; }
+
+        public SimpleArtificialPlayer()
+        {
+        }
+
+    }
+}
