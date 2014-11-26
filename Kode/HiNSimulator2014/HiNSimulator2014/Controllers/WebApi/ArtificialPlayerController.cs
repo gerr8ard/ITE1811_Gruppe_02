578e78e0f61b77798d79136ca7024b1fac43e464
aquister <alec90@gmail.com>
2014-11-26 11:29:43 +0100

    Kick og punch har nå innvirkning på score

--- Kode/HiNSimulator2014/HiNSimulator2014/Controllers/WebApi/ArtificialPlayerController.cs
+++ Kode/HiNSimulator2014/HiNSimulator2014/Controllers/WebApi/ArtificialPlayerController.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Globalization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Host.SystemWeb;
 using HiNSimulator2014.Models;
 using HiNSimulator2014.Classes;
 
@@ -11,11 +20,27 @@ namespace HiNSimulator2014.Controllers.WebApi
 {
     public class ArtificialPlayerController : ApiController
     {
+        private const int PUNCH_PROBABILITY = 35; // sannynlighet for vellykket slag
+        private const int KICK_PROBABILITY = 20; // sannsynlighet for vellykket spark
+
         private IRepository repository;
+        private ApplicationUserManager _userManager;
 
         public ArtificialPlayerController()
         {
             this.repository = new Repository();
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
 
         // GET: api/ArtificialPlayer/GetArtificialPlayersInLocation/5
@@ -43,8 +68,8 @@ namespace HiNSimulator2014.Controllers.WebApi
                 ImageID = ap.ImageID
             };
         }
-
-       // Returnerer en tilfeldig respons for en kunstig aktør
+        
+        // Returnerer en tilfeldig respons for en kunstig aktør
         [HttpGet]
         public String GetArtificialPlayerResponse(int id)
         {
@@ -56,5 +81,54 @@ namespace HiNSimulator2014.Controllers.WebApi
             }
             return null;
         }
+
+        // Returnerer en string som forteller hva som skjedde
+        [HttpGet]
+        public String PunchArtificialPlayer(int id)
+        {
+            ApplicationUser user = UserManager.FindById(User.Identity.GetUserId());
+            ArtificialPlayer ap = repository.GetArtificialPlayer(id);
+            Random r = new Random();
+
+            if (r.Next(0, 101) > PUNCH_PROBABILITY)
+            {
+                int scoreloss = 100 - PUNCH_PROBABILITY;
+                user.Score -= scoreloss;
+                UserManager.Update(user);
+                return "You underestimated " + ap.Name + ". He dodges and you hit yourself in the face. " + scoreloss + " was deducted from your score.";
+            }
+            else {
+                int scoregain = 100 - PUNCH_PROBABILITY;
+                user.Score += scoregain;
+                UserManager.Update(user);
+                return "You smacked " + ap.Name + " right in the face. " + scoregain + " was added to your score";
+            }
+        }
+
+        // Returnerer en string som forteller hva som skjedde
+        [HttpGet]
+        public String KickArtificialPlayer(int id)
+        {
+            ApplicationUser user = UserManager.FindById(User.Identity.GetUserId());
+            ArtificialPlayer ap = repository.GetArtificialPlayer(id);
+            Random r = new Random();
+
+            if (r.Next(0, 101) > KICK_PROBABILITY)
+            {
+                int scoreloss = (100 - PUNCH_PROBABILITY) * 3;
+                user.Score -= scoreloss;
+                UserManager.Update(user);
+                return "You tried to kick " + ap.Name + ", but the resitance was to strong. " + scoreloss + " was deducted from your score.";
+            }
+            else
+            {
+                int scoregain = (100 - PUNCH_PROBABILITY) * 2;
+                user.Score += scoregain;
+                UserManager.Update(user);
+                return "You kicked " + ap.Name + "s ass. " + scoregain + " was added to your score";
+            }
+        }
+
+
     }
 }
